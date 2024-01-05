a01c7059be1b491f57bf49168b955679698cbf80
Ritesh Raj <rajriteshkatihar@gmail.com>
2024-01-06 02:14:01 +0530

    Dashboard, category and cart
    Added dashboard, category and cart

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,26 @@
+import getbillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import Billboard from "@/components/billboard";
+import ProductList from "@/components/product-list";
+import Container from "@/components/ui/container"
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+
+    const products = await getProducts({ isFeatured: true});
+    const billboard = await getbillboard("0238ee60-f48c-47fc-8ef6-67a348ee1de6");
+
+    return ( 
+        <Container>
+            <div className="space-y-10 pb-10">
+                <Billboard data={billboard}/>
+                <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+                    <ProductList title="Featured Products" items={products} />
+                </div>
+            </div>
+        </Container>
+     );
+}
+ 
+export default HomePage;
---
+++ hooks/use-cart.ts
@@ -0,0 +1,38 @@
+import toast from "react-hot-toast";
+import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
+
+import { Product } from "@/types";
+
+interface CartStore {
+    items: Product[];
+    addItem: (data: Product) => void;
+    removeItem: (id: string) => void;
+    removeAll: () => void;
+};
+
+const usePreviewModal = create(
+    persist<CartStore>((set, get) => ({
+        items: [],
+        addItem: (data: Product) => {
+            const currentItems = get().items;
+            const existingItem = currentItems.find((item) => item.id === data.id);
+
+            if(existingItem) {
+                return toast("Item already in cart.");
+            }
+
+            set({items: [...get().items, data]});
+            toast.success("Item added to cart.");
+        },
+        removeItem: (id: string) => {
+            set({items: [...get().items.filter((item) => item.id !== id)]});
+            toast.success("Items removed from the cart.")
+        },
+        removeAll: () => set({items: []}),
+    }), {
+        name: "cart-storage",
+        storage: createJSONStorage(() => localStorage)
+    })
+)
+export default usePreviewModal;
