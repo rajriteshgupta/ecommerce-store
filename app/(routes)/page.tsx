282ba4ba866f971badb59f236b2a14829c1fd29e
Ritesh Raj <rajriteshkatihar@gmail.com>
2024-06-23 17:12:45 +0530

    BillboardId update

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -2,25 +2,24 @@ import getbillboard from "@/actions/get-billboard";
 import getProducts from "@/actions/get-products";
 import Billboard from "@/components/billboard";
 import ProductList from "@/components/product-list";
-import Container from "@/components/ui/container"
+import Container from "@/components/ui/container";
 
 export const revalidate = 0;
 
 const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getbillboard("6677f1680ee95a3f08796a99");
 
-    const products = await getProducts({ isFeatured: true});
-    const billboard = await getbillboard("3e11b486-d379-4663-9af3-4859d5fea34d");
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
+      </div>
+    </Container>
+  );
+};
 
-    return ( 
-        <Container>
-            <div className="space-y-10 pb-10">
-                <Billboard data={billboard}/>
-                <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-                    <ProductList title="Featured Products" items={products} />
-                </div>
-            </div>
-        </Container>
-     );
-}
- 
 export default HomePage;
